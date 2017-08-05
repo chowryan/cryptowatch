a6bd01775ec94111efb2d0020b989663c96382cb
kluwena <karelluwena@gmail.com>
2017-08-05 15:11:46 -0700

    moved menu to own component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,38 +1,7 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
-import {
-  Button,
-  Container,
-  Divider,
-  Grid,
-  Header,
-  Icon,
-  Image,
-  List,
-  Menu,
-  Segment,
-  Visibility,
-} from 'semantic-ui-react'
 
-const FixedMenu = () => (
-  <Menu fixed='top' size='large'>
-    <Container>
-      <Menu.Item as='a' active>Home</Menu.Item>
-      <Menu.Item as='a'>Work</Menu.Item>
-      <Menu.Item as='a'>Company</Menu.Item>
-      <Menu.Item as='a'>Careers</Menu.Item>
-      <Menu.Menu position='right'>
-        <Menu.Item className='item'>
-          <Button as='a'>Log in</Button>
-        </Menu.Item>
-        <Menu.Item>
-          <Button as='a' primary>Sign Up</Button>
-        </Menu.Item>
-      </Menu.Menu>
-    </Container>
-  </Menu>
-)
-
+import FixedMenu from './Menu'
 import News from './News';
 
 class App extends Component {
@@ -58,7 +27,6 @@ class App extends Component {
     return (
       <div>
         <FixedMenu />
-        <h1>homepage</h1>
         <News />
       </div>
     );

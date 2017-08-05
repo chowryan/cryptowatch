49138617f38fa797c91087553394abe567b8e235
kluwena <karelluwena@gmail.com>
2017-08-05 11:08:05 -0700

    twitter helper

--- server/api/twitter.js
+++ server/api/twitter.js
@@ -1,2 +1,26 @@
-const Twitter = require('twitter-node-client').Twitter;
+const keys = require('../../config/apiKeys')
 
+exports.tweetSearch = function(query, res) {
+  const error = function(err, res, body) {
+    console.log('ERROR in Twitter Fetch [%s]', err);
+  };
+  const success = function (data) {
+    console.log('Fetch from Twitter Successful');
+    var result = data;
+    res.send('hello from twitter search')
+  };
+
+  const Twitter = require('twitter-node-client').Twitter;
+
+  const config = {
+    "consumerKey": keys.Twitter.consumerKey,
+    "consumerSecret": keys.Twitter.consumerSecret,
+    "accessToken": keys.Twitter.accessToken,
+    "accessTokenSecret": keys.Twitter.accessSecret,
+    "callBackUrl": 'None'
+  }
+
+  const twitter = new Twitter(config);
+
+  twitter.getSearch({'q':'#haiku','count': 10}, error, success);
+}

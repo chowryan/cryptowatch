526266a87e2f1234ec834839a84d47d2226ddfc2
Yzhang1994 <yzhang1994@gmail.com>
2017-08-05 11:23:44 -0700

    add watsonhelpers

--- server/app.js
+++ server/app.js
@@ -3,12 +3,32 @@ const cors = require('cors');
 const express = require('express');
 const path = require('path');
 
+const { analyzeSentiment } = require('./watsonHelpers');
+
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public/dist')));
+
+app.get('/watson', (req, res) => {
+  const sampleParams = {
+    html: '<html><head><title>Fruits</title></head><body><h1>Apples and Oranges</h1><p>I love apples! I don\'t like oranges.</p></body></html>',
+    features: {
+      emotion: {
+        targets: ['apples', 'oranges'],
+      },
+    },
+  };
+  analyzeSentiment(sampleParams)
+  .then((data) => {
+    res.send(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+});
 
 app.post('/', (req, res) => {
   res.status(201).send(req.body);

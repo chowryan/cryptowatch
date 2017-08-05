38a7e3e602b1eee1ed9c275149ebcf619528228e
Carlo P. Las Marias <carlol@gmail.com>
2017-08-05 11:24:17 -0700

    utils folder

--- client/src/utils/gdax.js
+++ client/src/utils/gdax.js
@@ -1,2 +1,20 @@
-const orderbookSync = new Gdax.OrderbookSync(['BTC-USD', 'ETH-USD']);
-console.log(orderbookSync.books['ETH-USD'].state());
+
+
+const socket = require('socket.io-client')('wss://ws-feed.gdax.com');
+
+const auth = {
+  type: 'subscribe',
+  product_ids: [
+    'BTC-USD'
+  ],
+  signature: '',
+  key: '',
+  passphrase: '',
+  timestamp: '',
+};
+
+socket.emit('subscribe', { my: 'data' });
+console.log(socket);
+socket.on('connect', () => console.log(socket));
+socket.on('event', (data) => console.log(data));
+socket.on('disconnect', () => console.log('disconnected'));

97617f40c654b11fe20d25d964af2af9ac6ed914
Yzhang1994 <yzhang1994@gmail.com>
2017-08-04 21:17:28 -0700

    add more dependencies and linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-    "extends": "airbnb"
+  "extends": "airbnb",
+  "plugins": [
+    "react",
+    "import"
+  ],
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+  },
 };

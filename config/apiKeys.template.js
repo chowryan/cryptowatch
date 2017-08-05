8ceef6df18c3a527e0ab7d28730ec0143c5629bd
kluwena <karelluwena@gmail.com>
2017-08-05 10:39:28 -0700

    init config apikeys | twitter node module

---
+++ config/apiKeys.template.js
@@ -0,0 +1,12 @@
+module.exports = {
+  watson: {
+    username: 'WATSON_USERNAME_HERE',
+    password: 'WATSON_PASSWORD_HERE',
+  },
+  twitter: {
+    consumer_key: 'TWITTER_CONSUMER_KEY',
+    consumer_secret: 'TWITTER_CONSUMER_SECRET',
+    access_token_key: 'TWITTER_ACCESS_KEY',
+    access_token_secret: 'TWITTER_ACCESS_SECRET',
+  },
+};

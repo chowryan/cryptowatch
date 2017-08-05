208e26dfca9c5fd1feb8c8d86b8c180e02863c60
Carlo P. Las Marias <carlol@gmail.com>
2017-08-04 21:17:09 -0700

    initial react commit

---
+++ client/src/app.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Redux config
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// End Redux
+import reducers from './reducers';
+
+// createLogger can be turned off for product
+const middleware = [thunk, createLogger()];
+
+const composeEnhancers =
+  process.env.NODE_ENV !== 'production' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      name: 'app', actionBlacklist: ['REDUX_STORAGE_SAVE'],
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware),
+  // other store enhancers if any
+);
+
+const store = createStore(reducers, enhancer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    {element()}
+  </Provider>
+  document.getElementById('app'),
+);

e2d39bd19d6126d62eafc3c81faa1f8490e9a40f
Yzhang1994 <yzhang1994@gmail.com>
2017-08-05 21:54:47 -0700

    established mood score api for twitter

--- server/app.js
+++ server/app.js
@@ -4,37 +4,28 @@ const express = require('express');
 const path = require('path');
 const getTweets = require('./api/twitter');
 
-const { analyzeSentiment } = require('./watsonHelpers');
-
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public/dist')));
-app.get('/searchTwitter', (req, res) => {
-  // const searchText = req.body.text;
-  const searchText = 'bitcoin';
-  getTweets(searchText, (emotions, allTweets) => {
-    console.log('emotions: ', emotions);
-    console.log('allTweets: ', allTweets);
-    let searchResult = {
-      sentimentScore: emotions.joy * 4 - emotions.sadness - emotions.anger - emotions.disgust - emotions.fear,
-      tweets: allTweets,
-    }
-    res.send(searchResult);
-  })
- });
 
-app.get('/watson', (req, res) => {
-  const text = 'bitcoin good, ethereum bad';
-  analyzeSentiment(text)
-  .then((data) => {
-    res.send(data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+app.get('/retrieveCryptoMood/:source/:keyword', (req, res) => {
+  if (req.params.source === 'twitter') {
+    const searchText = req.params.keyword;
+    getTweets(searchText, (emotions, allTweets) => {
+      const { joy, sadness, anger, disgust, fear } = emotions;
+      const searchResult = {
+        keyword: searchText,
+        sentimentScore: (joy * 4) - sadness - anger - disgust - fear,
+        tweets: allTweets,
+      };
+      res.send(searchResult);
+    });
+  } else if (req.params.source === 'reddit') {
+    res.send({ error: 'currently unavailble' });
+  } else { res.send({ error: 'invalid source' }); }
 });
 
 app.post('/', (req, res) => {

9bbbb4579c4431ff712a01f6d399783863321eee
Yzhang1994 <yzhang1994@gmail.com>
2017-08-05 20:42:24 -0700

    allow reddit sorting

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-
-import { getRedditPosts } from '../utils/reddit';
-import { Grid, Table, Image } from 'semantic-ui-react';
-
+import { Grid, Table, Image, Button, Loader, Dimmer } from 'semantic-ui-react';
 import './css/News.css'
+import { getRedditPosts } from '../utils/reddit';
 import {
   updateNews,
 } from '../actions';
@@ -25,30 +23,42 @@ class News extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sortBy: 'hot',
-      subreddit: 'cryptocurrency',
+      loading: true,
     };
+    this.updateReddit = this.updateReddit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
+    this.updateReddit('top');
+  }
+
+  updateReddit(sortParam) {
+    this.setState({ loading: true });
     const { updateNews } = this.props;
+    const subreddit = 'cryptocurrency';
+    const sortBy = sortParam;
     const count = 0;
     const limit = 100;
-    return getRedditPosts(this.state.subreddit, this.state.sortBy, count, limit)
+    return getRedditPosts(subreddit, sortBy, count, limit)
     .then((res) => {
       updateNews(res.data.slice(1));
+      this.setState({ loading: false });
     })
     .catch((err) => {
       console.log(err);
     });
   }
 
+  handleClick(e, obj) {
+    this.updateReddit(obj.children.toLowerCase());
+  }
 
   render() {
     const { news } = this.props;
-    const divStyle= {
+    const divStyle = {
       overflow: 'auto',
-      height: '100vh'
+      height: '100vh',
     };
     return (
       <div>
@@ -58,18 +68,31 @@ class News extends Component {
               <Table padded>
                 <Table.Header size="large">
                   <Table.Row>
-                    <Table.HeaderCell textAlign='center' colSpan='2'>Reddit</Table.HeaderCell>
+                    <Table.HeaderCell textAlign="center" colSpan="2">
+                      <span style={{ marginRight: 20 }}>Reddit</span>
+                      <Button onClick={this.handleClick} color="green">Top</Button>
+                      <Button onClick={this.handleClick} color="red">Hot</Button>
+                      <Button onClick={this.handleClick} color="violet">New</Button>
+                    </Table.HeaderCell>
                   </Table.Row>
                 </Table.Header>
                 <Table.Body>
-                <div style={divStyle}>
+                {this.state.loading ?
+                  <Table.Row style={{ height: '300vh' }}>
+                    <Dimmer active inverted>
+                      <Loader inverted>loading</Loader>
+                    </Dimmer>
+                  </Table.Row> :
+                  <div style={divStyle}>
                     {news.map((item, i) => (
                       <Table.Row key={i}>
-                        <Table.Cell><Image src={item.image} size="mini" shape='rounded'/></Table.Cell>
+                        <Table.Cell>
+                          <Image src={item.image} size="mini" shape="rounded" />
+                        </Table.Cell>
                         <Table.Cell><a href={item.url}>{item.title}</a></Table.Cell>
                       </Table.Row>
                     ))}
-                </div>
+                  </div>}
                 </Table.Body>
               </Table>
             </Grid.Column>
@@ -77,7 +100,7 @@ class News extends Component {
               <Table padded>
                 <Table.Header size="large">
                   <Table.Row>
-                    <Table.HeaderCell textAlign='center' colSpan='2'>Twitter</Table.HeaderCell>
+                    <Table.HeaderCell textAlign="center" colSpan="2">Twitter</Table.HeaderCell>
                   </Table.Row>
                 </Table.Header>
                 <Table.Body>

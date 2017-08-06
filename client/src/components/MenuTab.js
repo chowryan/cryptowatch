0d4d0c198f126b4bc1281e6720518cc453a5640c
kluwena <karelluwena@gmail.com>
2017-08-05 17:08:54 -0700

    created menu tab

---
+++ client/src/components/MenuTab.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Tab } from 'semantic-ui-react'
+
+import News from './News';
+
+const panes = [
+  { menuItem: 'Home', render: () => <Tab.Pane attached={false}>*Home page here*</Tab.Pane> },
+  { menuItem: 'Order Book', render: () => <Tab.Pane attached={false}>*Order Book Here*</Tab.Pane> },
+  { menuItem: 'Analytics', render: () => <Tab.Pane attached={false}>*Analytics here*</Tab.Pane> },
+  { menuItem: 'News', render: () => <Tab.Pane attached={false}><News /></Tab.Pane> },
+]
+
+const MenuTab = () => (
+  <Tab size="large" menu={{inverted: true, size: "large", attached: false}} panes={panes} />
+)
+
+export default MenuTab

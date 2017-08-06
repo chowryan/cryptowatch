a2a681166285b96d90779d4ca758cfe98e875ed2
kluwena <karelluwena@gmail.com>
2017-08-06 09:06:59 -0700

    livedatatable reformatting fix

--- client/src/components/LiveDataTable.js
+++ client/src/components/LiveDataTable.js
@@ -70,64 +70,6 @@ class LiveDataTable extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-        <Table celled selectable color="violet">
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell singleLine>Exchanges</Table.HeaderCell>
-              <Table.HeaderCell>Bitcoin</Table.HeaderCell>
-              <Table.HeaderCell>Ethereum</Table.HeaderCell>
-              <Table.HeaderCell>Litecoin</Table.HeaderCell>
-              <Table.HeaderCell>Ripple</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-
-          <Table.Body>
-            <Table.Row>
-              <Table.Cell>Bitstamp</Table.Cell>
-              <Table.Cell positive={this.state.BTC_Bitstamp_p} negative={this.state.BTC_Bitstamp_n}>{this.state.BTC_Bitstamp}</Table.Cell>
-              <Table.Cell disabled>{this.state.ETH_Bitstamp}</Table.Cell>
-              <Table.Cell positive={this.state.LTC_Bitstamp_p} negative={this.state.LTC_Bitstamp_n}>{this.state.LTC_Bitstamp}</Table.Cell>
-              <Table.Cell positive={this.state.XRP_Bitstamp_p} negative={this.state.XRP_Bitstamp_n}>{this.state.XRP_Bitstamp}</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>GDAX</Table.Cell>
-              <Table.Cell positive={this.state.BTC_GDAX_p} negative={this.state.BTC_GDAX_n}>{this.state.BTC_GDAX}</Table.Cell>
-              <Table.Cell positive={this.state.ETH_GDAX_p} negative={this.state.ETH_GDAX_n}>{this.state.ETH_GDAX}</Table.Cell>
-              <Table.Cell positive={this.state.LTC_GDAX_p} negative={this.state.LTC_GDAX_n}>{this.state.LTC_GDAX}</Table.Cell>
-              <Table.Cell disabled>{this.state.XRP_GDAX}</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>Kraken</Table.Cell>
-              <Table.Cell disabled>{this.state.BTC_Kraken}</Table.Cell>
-              <Table.Cell positive={this.state.ETH_Kraken_p} negative={this.state.ETH_Kraken_n}>{this.state.ETH_Kraken}</Table.Cell>
-              <Table.Cell positive={this.state.LTC_Kraken_p} negative={this.state.LTC_Kraken_n}>{this.state.LTC_Kraken}</Table.Cell>
-              <Table.Cell positive={this.state.XRP_Kraken_p} negative={this.state.XRP_Kraken_n}>{this.state.XRP_Kraken}</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>CEX.IO</Table.Cell>
-              <Table.Cell positive={this.state.BTC_cexio_p} negative={this.state.BTC_cexio_n}>{this.state.BTC_cexio}</Table.Cell>
-              <Table.Cell positive={this.state.ETH_cexio_p} negative={this.state.ETH_cexio_n}>{this.state.ETH_cexio}</Table.Cell>
-              <Table.Cell disabled>{this.state.LTC_cexio}</Table.Cell>
-              <Table.Cell disabled>{this.state.XRP_cexio}</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>POLONIEX</Table.Cell>
-              <Table.Cell positive={this.state.BTC_POLONIEX_p} negative={this.state.BTC_POLONIEX_n}>{this.state.BTC_POLONIEX}</Table.Cell>
-              <Table.Cell positive={this.state.ETH_POLONIEX_p} negative={this.state.ETH_POLONIEX_n}>{this.state.ETH_POLONIEX}</Table.Cell>
-              <Table.Cell positive={this.state.LTC_POLONIEX_p} negative={this.state.LTC_POLONIEX_n}>{this.state.LTC_POLONIEX}</Table.Cell>
-              <Table.Cell positive={this.state.XRP_POLONIEX_p} negative={this.state.XRP_POLONIEX_n}>{this.state.XRP_POLONIEX}</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>GEMINI</Table.Cell>
-              <Table.Cell positive={this.state.BTC_GEMINI_p} negative={this.state.BTC_GEMINI_n}>{this.state.BTC_GEMINI}</Table.Cell>
-              <Table.Cell positive={this.state.ETH_GEMINI_p} negative={this.state.ETH_GEMINI_n}>{this.state.ETH_GEMINI}</Table.Cell>
-              <Table.Cell disabled>{this.state.LTC_GEMINI}</Table.Cell>
-              <Table.Cell disabled>{this.state.XRP_GEMINI}</Table.Cell>
-            </Table.Row>
-          </Table.Body>
-        </Table>
-=======
         <Container>
           <Table celled selectable color='purple'>
             <Table.Header>
@@ -185,7 +127,6 @@ class LiveDataTable extends Component {
             </Table.Body>
           </Table> 
         </Container>
->>>>>>> add container
       </div>
     );
   }

7ee3ca964f0f80c4c366d4cd80ad110fda16cfa0
kluwena <karelluwena@gmail.com>
2017-08-05 17:10:59 -0700

    refactor menu to semantic tabs

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,29 +1,77 @@
 import React, { Component } from 'react';
-import {
-  Button,
-  Icon,
-  Container,
-  Menu,
-} from 'semantic-ui-react'
+import { Button, Icon, Container, Menu, Tab } from 'semantic-ui-react'
 
-const FixedMenu = () => (
-  <Menu inverted fixed='top' size='large'>
-    <Container>
-      <Menu.Item header><Icon circular inverted name="eye"/></Menu.Item>
-      <Menu.Item as='a'>Home</Menu.Item>
-      <Menu.Item as='a'>Order Book</Menu.Item>
-      <Menu.Item as='a'>Analytics</Menu.Item>
-      <Menu.Item as='a'>News</Menu.Item>
-      <Menu.Menu position='right'>
-        <Menu.Item className='item'>
-          <Button as='a'>Log in</Button>
-        </Menu.Item>
-        <Menu.Item>
-          <Button as='a' primary>Sign Up</Button>
-        </Menu.Item>
-      </Menu.Menu>
-    </Container>
-  </Menu>
-)
+import {
+  openHome,
+  closeHome,
+  openOrder,
+  closeOrder,
+  openAnalytics,
+  closeAnalytics,
+  openNews,
+  closeNews
+} from '../actions/index.js'
+
+class FixedMenu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleHome = this.toggleHome.bind(this);
+    this.toggleOrder = this.toggleOrder.bind(this);
+    this.toggleAnalytics = this.toggleAnalytics.bind(this);
+    this.toggleNews= this.toggleNews.bind(this);
+  }
+
+  toggleHome() {
+    this.props.openHome();
+    this.props.closeOrder();
+    this.props.closeAnalytics();
+    this.props.closeNews();
+  }
+
+  toggleOrder() {
+    this.props.openOrder();
+    this.props.closeHome();
+    this.props.closeAnalytics();
+    this.props.closeNews();
+  }
+
+  toggleAnalytics() {
+    this.props.openAnalytics();
+    this.props.closeHome();
+    this.props.closeOrder();
+    this.props.closeNews();
+  }
+
+  toggleNews() {
+    this.props.openNews();
+    this.props.closeHome();
+    this.props.closeOrder();
+    this.props.closeAnalytics();
+  }
+
+render() {
+  return (
+    <Menu inverted fixed='top' size='large'>
+      <Container>
+        <Menu.Item name="eye">crypto<Icon name="eye"/></Menu.Item>
+        <Menu.Item as='a'>Home</Menu.Item>
+        <Menu.Item as='a'>Order Book</Menu.Item>
+        <Menu.Item as='a'>Analytics</Menu.Item>
+        <Menu.Item as='a'>News</Menu.Item>
+        <Menu.Menu position='right'>
+          <Menu.Item className='item'>
+            <Button as='a'>Log in</Button>
+          </Menu.Item>
+          <Menu.Item>
+            <Button as='a' primary>Sign Up</Button>
+          </Menu.Item>
+        </Menu.Menu>
+      </Container>
+    </Menu>
+    );
+  }
+}
+
 
 export default FixedMenu;
